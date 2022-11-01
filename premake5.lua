1676aaebc663f3cf8f2db6f6e6bebf4aeadaf3ba
Muhammad Aman Yasin <M.AMANYASIN@GMAIL.COM>
2022-11-01 23:28:24 +0200

    imgui premake

--- premake5.lua
+++ premake5.lua
@@ -8,15 +8,17 @@ project "ImGui"
 	files
 	{
 		"imconfig.h",
-		"imgui.h",
 		"imgui.cpp",
+		"imgui.h",
+		"imgui.natvis",
+		"imgui_demo.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
-		"imgui_widgets.cpp",
+		"imgui_tables.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_widgets.cpp",
 	}
 
 	filter "system:windows"
